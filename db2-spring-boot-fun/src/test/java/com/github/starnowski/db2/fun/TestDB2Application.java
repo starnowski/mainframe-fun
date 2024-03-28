2328ef471f7809d75f0e75689880d7a05d21dfca
starnowski <33316705+starnowski@users.noreply.github.com>
2024-03-28 01:42:41 +0100

    Fixed tests configuration

---
+++ db2-spring-boot-fun/src/test/java/com/github/starnowski/db2/fun/TestDB2Application.java
@@ -0,0 +1,11 @@
+package com.github.starnowski.db2.fun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDB2Application {
+    public static void main(String ... args) {
+        SpringApplication.run(TestDB2Application.class, args);
+    }
+}
