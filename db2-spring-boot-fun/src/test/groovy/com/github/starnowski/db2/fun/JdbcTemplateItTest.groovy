aa6cd1e3871bcc18cf3721e36873035d479d5a32
starnowski <33316705+starnowski@users.noreply.github.com>
2024-03-28 09:15:19 +0100

    Added tess for store procedure

--- db2-spring-boot-fun/src/test/groovy/com/github/starnowski/db2/fun/JdbcTemplateItTest.groovy
+++ db2-spring-boot-fun/src/test/groovy/com/github/starnowski/db2/fun/JdbcTemplateItTest.groovy
@@ -5,6 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.dao.DataAccessException
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.core.PreparedStatementCallback
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
+import org.springframework.jdbc.core.namedparam.SqlParameterSource
+import org.springframework.jdbc.core.simple.SimpleJdbcCall
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.jdbc.JdbcTestUtils
@@ -53,7 +56,7 @@ class JdbcTemplateItTest extends Specification {
             executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(value = "clear_tables.sql",
             executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-    def "should save item with number to db2 table with name #name and number"() {
+    def "should save item with number to db2 table with name #name and number #number"() {
         given:
             final String itemName = name
             final int itemNumber = number
@@ -79,4 +82,33 @@ class JdbcTemplateItTest extends Specification {
             name | number
             "xxx"   | 13
     }
+
+    @Sql(value = ["clear_tables.sql", "items_with_number_add_upsert_procedure.sql"],
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = ["clear_tables.sql", "items_with_number_drop_upsert_procedure.sql"],
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    def "should insert item with number to db2 table with name #name and number #number with procedure"() {
+        given:
+            final String itemName = name
+            final int itemNumber = number
+            SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate).withSchemaName("DB2_FUN").withProcedureName("ITEMS_WITH_NUMBER_UPSERT")
+            SqlParameterSource parameterSource = new MapSqlParameterSource().addValue("P_ITEM_NAME", name).addValue("P_NUM", number)
+
+        when:
+            simpleJdbcCall.execute()
+
+        then:
+            JdbcTestUtils.countRowsInTable(jdbcTemplate, "DB2_FUN.ITEMS_WITH_NUMBER") == 1
+            // Same assertion but with usage of groovy string templates
+            JdbcTestUtils.countRowsInTableWhere(jdbcTemplate, "DB2_FUN.ITEMS_WITH_NUMBER", "ITEM_NAME = '${name}' AND NUM = ${itemNumber}") == 1
+
+        where:
+            name                | number
+            "vvcc"              | 13
+            "some item name"    | 463
+    }
+
+
+    //items_with_number_add_upsert_procedure.sql
+    //items_with_number_drop_upsert_procedure.sql
 }
